94cc57e744174cedae8eabda692c31364fcef5f2
ORAU <heymandameunmail@gmail.com>
2025-06-11 19:54:17 -0600

    Using windows and tabs

--- tests/CustomTests/UIBasicTests2.spec.js
+++ tests/CustomTests/UIBasicTests2.spec.js
@@ -24,3 +24,23 @@ await passwordTxt.fill('learning');
 await loginBtn.click();
 
 });
+
+test.only('Using different window tabs',async({browser})=>{
+const context=await browser.newContext();
+const page=await context.newPage();
+await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+const usernameTxt=page.locator('#username');
+const passwordTxt=page.locator('#password');
+const documentLink=page.locator("[href*='documents-request']");
+const [newPage]=await Promise.all([
+    context.waitForEvent('page'),
+    documentLink.click()
+]);
+const text=await newPage.locator(".red").textContent();
+const arrayTexts=text.split("@");
+const domain=arrayTexts[1].split(" ");
+console.log(domain[0]);
+await usernameTxt.fill(domain[0]);
+await passwordTxt.fill('learning');
+await page.pause();
+});
