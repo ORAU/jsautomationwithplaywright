e2f965a76d2cb5e6c44624903fc02a463424f1ca
ORAU <heymandameunmail@gmail.com>
2025-07-19 23:49:26 -0600

    Implementó getByRole methods

--- tests/CustomTests/GetByLabelPractice.spec.js
+++ tests/CustomTests/GetByLabelPractice.spec.js
@@ -5,8 +5,10 @@ test('Get ByLabel Practice workflow',async({page})=>{
 await page.goto('https://rahulshettyacademy.com/angularpractice/');
 await page.getByLabel("Check me out if you Love IceCreams!").click();
 await page.getByLabel("Employed").check();
-await page.getByLabel("Gender").selectOption("Male"); 
-
-
+await page.getByLabel("Gender").selectOption("Female"); 
+await page.getByPlaceholder("Password").fill("Contrasena");
+await page.locator('form input[name="name"]').fill("Orlando");
+await page.locator('form input[name="email"]').fill("test_email@domain.com");
+await page.getByRole("button",{name:'Submit'}).click();
 });
 
