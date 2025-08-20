6f5d7222cabae7d9a6eef5fb7347558f85925b59
ORAU <heymandameunmail@gmail.com>
2025-08-20 00:05:41 -0600

    Get by role implementation part 2

--- tests/CustomTests/GetByLabelPractice2.spec.js
+++ tests/CustomTests/GetByLabelPractice2.spec.js
@@ -14,12 +14,18 @@ await page.waitForLoadState('networkidle');
 await page.locator(".card-body b").first().waitFor();
 await page.locator(".card-body").filter({hasText:productName}).getByRole('button',{name:"Add to Cart"}).click();
 await page.getByRole("listitem").getByRole("button",{name:"  Cart "}).click();
-//await page.pause();
+await page.pause();
 await page.locator("div li").first().waitFor();
 await expect(page.getByText(productName)).toBeVisible();
 await page.getByRole("button", { name: "Checkout" }).click();
+await page.pause();
+//await page.getByPlaceholder("Select Country").fill("El Salvador");
+await page.getByPlaceholder("Select Country").pressSequentially("El Salvador");
+await page.getByRole("button", { name: "El Salvador" }).click();
+await page.getByRole("button", { name: "Place Order" }).click();
+await expect(page.getByText("Thank you for the order.")).toBeVisible();
+
 //await page.getByPlaceholder().fill();
 //await page.getByRole("link",{name:'Shop'}).click();
 
 });
-
